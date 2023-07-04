f38a75f26dc90025a7e44c2d8f291945eced0af8
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-07-05 00:55:05 +0200

    WIP adjust to ComponentFactory

--- Resources/Private/Helper.php
+++ Resources/Private/Helper.php
@@ -4,26 +4,27 @@ declare(strict_types=1);
 
 namespace PackageFactory\FusionFactory\Application;
 
+use Neos\Flow\Mvc\Controller\ControllerContext;
 use Neos\Fusion\Core\RuntimeFactory;
 use Neos\Neos\Domain\Model\SiteNodeName;
 use Neos\Neos\Domain\Repository\SiteRepository;
 use Neos\Neos\Domain\Service\FusionService;
 use PackageFactory\ComponentFactory\Application\RenderingStuff;
 use PackageFactory\ComponentFactory\Domain\ComponentFactory;
-use PackageFactory\ComponentFactory\Domain\CurrentControllerContext;
+use Neos\Flow\Mvc;
 use PackageFactory\FusionFactory\Domain\FusionObjectDto;
 
-function component(string $name, array $props): FusionObjectDto
+function h(string $type, array $props): FusionObjectDto
 {
-    return new FusionObjectDto($name, $props);
+    return new FusionObjectDto($type, $props);
 }
 
-function fusionRenderer(\Closure|ComponentFactory $factory): ComponentFactory
+function fusionComponent(\Closure|ComponentFactory $factory): ComponentFactory
 {
     if ($factory instanceof \Closure) {
         $factory = ComponentFactory::fromClosure($factory);
     }
-    return $factory->wrap(function (FusionObjectDto $output, RenderingStuff $renderingStuff) {
+    return $factory->compose(function (FusionObjectDto $output, RenderingStuff $renderingStuff) {
         return render($output, $renderingStuff);
     });
 }
@@ -36,10 +37,20 @@ function render(FusionObjectDto $fusionObjectDto, RenderingStuff $renderingStuff
     $site = $renderingStuff->di->get(SiteRepository::class)->findOneByNodeName(SiteNodeName::fromNodeName($renderingStuff->siteNode->nodeName))
         ?? throw new \Exception(sprintf('No site found for nodeNodeName "%s"', $renderingStuff->siteNode->nodeName->value), 1677245517);
 
+    $actionRequest = Mvc\ActionRequest::fromHttpRequest($renderingStuff->request);
+    $uriBuilder = new Mvc\Routing\UriBuilder();
+    $uriBuilder->setRequest($actionRequest);
+    $controllerContext = new ControllerContext(
+        $actionRequest,
+        new Mvc\ActionResponse(),
+        new Mvc\Controller\Arguments(),
+        $uriBuilder
+    );
+
     // todo cache runtime ...
     $runtime = $renderingStuff->di->get(RuntimeFactory::class)->createFromConfiguration(
         $renderingStuff->di->get(FusionService::class)->createFusionConfigurationFromSite($site),
-        CurrentControllerContext::get()
+        $controllerContext
     );
 
     $runtime->setEnableContentCache(false);
